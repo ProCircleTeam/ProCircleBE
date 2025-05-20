01b2c310cb8fb9ba4db8478c0fe9a8fb5b65872d
balogun biola <balogunbiola101@gmail.com>
2025-05-20 07:45:02 +0100

    new fix

--- src/services/users/users.hooks.ts
+++ src/services/users/users.hooks.ts
@@ -3,6 +3,7 @@ import { userDataValidator } from './users.schema'
 import { authenticate } from '@feathersjs/authentication'
 import { HookContext } from '@feathersjs/feathers'
 import { BadRequest } from '@feathersjs/errors'
+import type { Knex } from 'knex'
 
 const checkEmailExists = async (context: HookContext) => {
   const { app, data } = context
@@ -16,6 +17,34 @@ const checkEmailExists = async (context: HookContext) => {
     throw new BadRequest('This email is already registered')
   }
   
+  return context
+}
+
+const assignCodeName = async (context: HookContext) => {
+  const { app } = context
+  const knex: Knex = app.get('postgresqlClient')
+
+  // Start a transaction since we need to make multiple updates atomically
+  await knex.transaction(async (trx) => {
+    // Get a random unassigned code name
+    const codeName = await trx('code_names')
+      .where({ assigned: false })
+      .orderByRaw('RANDOM()')
+      .first()
+
+    if (!codeName) {
+      throw new BadRequest('No code names available')
+    }
+
+    // Mark the code name as assigned
+    await trx('code_names')
+      .where({ id: codeName.id })
+      .update({ assigned: true })
+
+    // Assign the code name to the user
+    context.data.code_name_id = codeName.id
+  })
+
   return context
 }
 
@@ -33,7 +62,7 @@ export const userHooks = {
     all: [],
     find: [],
     get: [],
-    create: [schemaHooks.validateData(userDataValidator), checkEmailExists],
+    create: [schemaHooks.validateData(userDataValidator), checkEmailExists, assignCodeName],
     patch: [],
     remove: []
   }
