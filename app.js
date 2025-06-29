b5143b9d0352d3c14f6b4360730501a870f2ec04
ife <ifeoluwakole@icloud.com>
2025-06-29 16:41:05 +0100

    Implement weekly goal pairing feature

--- app.js
+++ app.js
@@ -13,12 +13,13 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello, welcome to ProCircle BE");
 });
+
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/goal", goalRoutes);
 
 
-const PORT = process.env.APP_PORT || 5000;
+const PORT = process.env.APP_PORT || 5050;
 app.listen(PORT, () => {
   console.log(`Server started successfully at http://localhost:${PORT}`);
 });
