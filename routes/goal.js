1f408ef4d081bbcce8dcd9c4942feec30c698e15
xrole <xrolediamond@gmail.com>
2025-07-10 08:33:16 +0100

    fix get route conflict and get user goal by date authentication issue

--- routes/goal.js
+++ routes/goal.js
@@ -16,6 +16,9 @@ router.route("/").post( authenticateToken,  createGoal);
 // Get user goals with pagination
 router.route("/").get(authenticateToken, getUserGoals);
 
+// getting goals by date
+router.route('/by-date').get(authenticateToken, getUserGoalsByDate);
+
 // Get single goal by ID
 router.route("/:id").get( authenticateToken,  getGoalById);
 
@@ -25,7 +28,5 @@ router.route("/:id").put( authenticateToken, updateGoal);
 // Mark goals as completed
 router.route("/:id/complete").patch( authenticateToken,  markGoalsCompleted);
 
-// getting goals by date
-router.get('/by-date', getUserGoalsByDate);
 
 module.exports = router;
