de404d67d5bc7b1dd39a28b6f525172603dee7dc
xrole <xrolediamond@gmail.com>
2025-07-28 01:15:13 +0100

    create reusable generic api response create api status enum upate auth controller to use this apiresponse update goals controller to use this api response

---
+++ utils/apiResponse.js
@@ -0,0 +1,12 @@
+const { ResponseStatusEnum } = require("../constants/enums/responseStatus");
+
+
+function apiResponse({res, status, statusCode, message, data = null}) {
+  return res.status(statusCode).json({
+    status,
+    message,
+    data,
+  });
+}
+
+module.exports = { apiResponse, ResponseStatusEnum };
