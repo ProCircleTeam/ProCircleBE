d1db19514118c8ed5809ab82c37bf8896df74357
xrole <xrolediamond@gmail.com>
2025-08-05 15:00:10 +0100

    implement request password otp

---
+++ migrations/20250805110326-add-reset-fields-to-users.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Users', 'resetCode', {
+      type: Sequelize.STRING,
+    });
+    await queryInterface.addColumn('Users', 'resetCodeExpiration', {
+      type: Sequelize.DATE,
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Users', 'resetCode');
+    await queryInterface.removeColumn('Users', 'resetCodeExpiration');
+  }
+};
---
+++ utils/mailer.js
@@ -0,0 +1,31 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail', 
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+/**
+ * Send email using nodemailer
+ * @param {Object} options
+ * @param {string} options.to - Recipient email
+ * @param {string} options.subject - Email subject
+ * @param {string} options.text - Plain text body
+ * @param {string} options.html - HTML body (optional)
+ */
+const sendMail = async ({ to, subject, text, html }) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+    html,
+  };
+
+  return transporter.sendMail(mailOptions);
+};
+
+module.exports = { sendMail };
