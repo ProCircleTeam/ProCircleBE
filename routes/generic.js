c8d98a60208d8f6ed2257cec7bb7d3b2994016b1
Andrew Okoye <andrewinsoul@gmail.com>
2025-10-03 19:20:14 +0100

    create endpoint for clearing DB

--- routes/generic.js
+++ routes/generic.js
@@ -2,8 +2,10 @@
 const router = require('express').Router();
 const {
 	generic,
+	clearDBData,
 } = require('../controller/generic_controller');
 
 router.route('/request-otp').post(generic);
+router.route('/clear-db').delete(clearDBData);
 
 module.exports = router;
