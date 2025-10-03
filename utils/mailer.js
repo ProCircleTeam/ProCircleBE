f7386ceaa2d3f8052da13967889fde54c36a173d
xrole <xrolediamond@gmail.com>
2025-10-03 17:23:48 +0100

    force nodemailer to use IPV 4

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,11 +1,18 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-	service: 'gmail',
+	// Service: 'gmail',
+	host: 'smtp.gmail.com',
+	port: 587,
+	secure: false,
 	auth: {
 		user: process.env.EMAIL_USER,
 		pass: process.env.EMAIL_PASS,
 	},
+	tls: {
+		rejectUnauthorized: false,
+	},
+	family: 4,
 });
 
 /**
