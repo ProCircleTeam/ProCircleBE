c8e8f5a71ebb9b19ca2358188c1fbc67ac6fde82
xrole <xrolediamond@gmail.com>
2025-10-15 08:24:48 +0100

    create endpoint for creating accountability call create functionality to refresh google access token when needed

--- routes/calendar.js
+++ routes/calendar.js
@@ -3,6 +3,7 @@ const {
 	calendarOauthUrl,
 	exchangeToken,
 	userCalendarBusyPeriod,
+	createAccountabilityEvent,
 } = require('../controller/calendar_controller');
 const {authenticateToken} = require('../middleware/auth');
 const router = require('express').Router();
@@ -10,6 +11,8 @@ const router = require('express').Router();
 router.route('/busy').get(authenticateToken, userCalendarBusyPeriod);
 router.route('/calendar-url').get(authenticateToken, calendarOauthUrl);
 router.route('/calendar-oauth-redirect-url').get(exchangeToken);
+router
+	.route('/accountability-call')
+	.post(authenticateToken, createAccountabilityEvent);
 
 module.exports = router;
-
